6217ea88da75735cecd5478ae1c6362bba873f67
vichmi <vichmibg@mailbg>
2022-12-04 21:31:24 +0200

    Added check out page

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -7,12 +7,62 @@ export default function Cart() {
   const [items, setItems] = useState(getProducts());
 
   useEffect(() => {
-    // setItems(getProducts());
+    setItems(getProducts());
   }, []);
 console.log(items);
   return (
-    <div style={{textAlign: 'center'}}>
-        <h1>Check out page </h1>
+    <div style={{textAlign: 'center', width: '100%'}}>
+        <h1>Check out</h1>
+        <div className='d-flex flex-row justify-content-around' style={{width: '100%'}}>
+          <div className='d-flex justify-content-start' style={{justifySelf:'flex-start'}}>
+
+            <form className='bg-light d-flex flex-column' style={{borderRadius: '20px', padding: 40}} >
+              <div className='d-flex flex-column justify-content-center' style={{height: '100%'}}>
+                <label style={{marginTop: 20}} for="first-name">First name</label>
+                <input type='text' name='first-name' id='first-name' placeholder='First name' />
+                
+                <label for="second-name">Second name</label>
+                <input type='text' name='second-name' id='second-name' placeholder='Second name' />
+                
+                <label for="phone">Phone number</label>
+                <input type='text' name='phone' id='phone' placeholder='Phone number' />
+
+                <label for="address">Address</label>
+                <input type='text' name='address' id='address' placeholder='Address' />
+              </div>
+              <div className='d-flex justify-content-center' style={{marginBottom: 50}}>
+                <Button type='submit'>Order</Button>
+              </div>
+            </form>
+
+          </div>
+
+          <div className='d-flex' style={{justifySelf:'flex-end'}}>
+            <div className='bg-light' style={{height: '50vh', borderRadius: '20px', padding: 20}}>
+              {items.length == 0 ? <>No items</>: items.map((item, index) => {
+                return (
+                  <div key={index}>
+                    <img src={item.image} style={{marginLeft: 5, marginTop: 5, marginBottom: 5}} width={40} height={50} />
+                    <span>{truncateString(item.title, 50)}</span>
+                    <Button onClick={() => {
+                      removeProduct(item);
+                      setItems(getProducts())
+                    }} variant='danger'>X</Button>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+        </div>
     </div>
   )
+}
+
+
+function truncateString(str, num) {
+  if (str.length > num) {
+    return str.slice(0, num) + "...";
+  } else {
+    return str;
+  }
 }
